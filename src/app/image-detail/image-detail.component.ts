6e5a73f911b38ab069f0e515aea0db787dd5223d
stephen <stephenola21@gmail.com>
2023-10-10 13:30:55 +0100

    setting up image details page

--- src/app/image-detail/image-detail.component.ts
+++ src/app/image-detail/image-detail.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-image-detail',
   templateUrl: './image-detail.component.html',
   styleUrls: ['./image-detail.component.scss']
 })
-export class ImageDetailComponent {
+export class ImageDetailComponent implements OnInit {
 
+  id: string = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id') || '';
+  }
 }
