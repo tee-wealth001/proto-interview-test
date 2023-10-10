4a6ec5e873b53c1c3606d5d51281eae584f1c54c
stephen <stephenola21@gmail.com>
2023-10-10 14:00:12 +0100

    modifying subscription deprecated structure

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,27 +6,34 @@ import { ImageService } from '../image.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
   corridorData: any[] = [];
 
-  constructor(private imageService: ImageService) {}
+  constructor(private imageService: ImageService) { }
 
 
   ngOnInit(): void {
-    this.imageService.getImages().subscribe((data) => {
-      
-      this.corridorData = data;
-      this.corridorData = data.filter((item:any) => item.images !== undefined).sort((a:any, b:any) => {
-        return new Date(b.releaseDate).getTime() - new Date(a.releaseDate).getTime();
-      });
-      this.getImageUrl(this.corridorData);
+
+    // modifying subscription deprecated structure
+    this.imageService.getImages().subscribe({
+      next: (data) => {
+        this.corridorData = data;
+
+        this.corridorData = data.filter((item: any) => item.images !== undefined).sort((a: any, b: any) => {
+          return new Date(b.releaseDate).getTime() - new Date(a.releaseDate).getTime();
+        });
+        this.getImageUrl(this.corridorData);
+      },
+      error: () => {
+        alert('There was an error in retrieving data from the server');
+      }
     });
   }
 
-  getImageUrl(item:any) {
+  getImageUrl(item: any) {
 
-    return item.images?.find((image:any) => image.type === 'thumbnail').url;
+    return item.images?.find((image: any) => image.type === 'thumbnail').url;
   }
 
 }
