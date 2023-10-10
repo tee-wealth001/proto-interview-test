1a2adcafe8ec2fb328413d90e14f0927829bc99d
stephen <stephenola21@gmail.com>
2023-10-10 13:28:55 +0100

    loaded images in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../image.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  corridorData: any[] = [];
+
+  constructor(private imageService: ImageService) {}
+
+
+  ngOnInit(): void {
+    this.imageService.getImages().subscribe((data) => {
+      
+      this.corridorData = data;
+      this.corridorData = data.sort((a:any, b:any) => {
+        return new Date(b.releaseDate).getTime() - new Date(a.releaseDate).getTime();
+      });
+      this.getImageUrl(this.corridorData);
+    });
+  }
+
+  getImageUrl(item:any) {
+
+    return item.images?.find((image:any) => image.type === 'thumbnail').url;
+  }
 
 }
